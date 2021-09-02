ef3acc25ea254307261a727d8287dd38824b4442
jamie1077 <jm1077@sky.com>
2021-09-02 22:07:02 +0100

    fixed issue with error handling

--- app.js
+++ app.js
@@ -12,35 +12,38 @@ app.use('/static', express.static('public'));
 //Lets express know to use pug templates in the views folder
 app.set('view engine', 'pug');
 
+// GET the homepage and include the projects from the json file
 app.get('/', (req, res) => {
     res.render('index', { projects });
 });
 
+//GET the about page
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-/* GET recipe page. */
-app.get('/projects/:id', function(req, res, next) {
+// GET the project page based on the project id. 
+app.get('/projects/:id', (req, res) => {
   const projectId = req.params.id;
   const project = projects.find( ({ id }) => id === +projectId );
   
   if (project) {
     res.render('project', { project });
-  } else {
-    res.sendStatus(404);
+  } else{
+    res.render('page-not-found');
   }
 });
 
+
 /* ERROR HANDLERS */
 
-/* 404 handler to catch undefined or non-existent route requests */ 
+// 404 handler to catch undefined or non-existent route requests
 app.use((req, res, next) => {
   console.log('404 error handler called');
   res.status(404).render('page-not-found');
 });
 
-/* Global error handler */
+// Global error handler 
 app.use((err, req, res, next) => {
   if (err) {
     console.log('Global error handler called', err);
@@ -49,8 +52,8 @@ app.use((err, req, res, next) => {
   if(err.status === 404) {
     res.status(404).render('page-not-found', {err});
   }else{
-    err.message = err.message;
-    res.status(err.status).render('error', {err});
+    err.message = err.message || `Oops! Looks like something went wrong on the server`;
+    res.status(err.status || 500).render('error', {err});
   }
 });
 
